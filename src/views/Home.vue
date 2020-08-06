db5ee83473ac9928338b29fabcedc6850943526d
oatzapao56@gmail.com <oatzapao56@gmail.com>
2020-08-06 16:18:47 +0700

    4

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    
+    <v-btn color="primary">HOME</v-btn>
+    <v-btn color="error">Error</v-btn>
+
+     <v-btn class="mx-2" fab dark small color="pink">
+      <v-icon dark>mdi-heart</v-icon>
+    </v-btn>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
+
+<style>
+
+</style>
