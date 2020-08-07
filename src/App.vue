995785c4bc49cdedd267c3d297873bff4497e87b
Phuminan Wimano <oatzapao56@gmail.com>
2020-08-07 14:37:25 +0700

    5]Edit Navigation Drawers

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,40 @@
 
   <v-app>
     
-    <v-navigation-drawer app>
-      <!-- -->
-      <p>navigation drawer</p>
-    </v-navigation-drawer>
+      <v-navigation-drawer app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
 
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  
     <v-app-bar app>
       <!-- -->
       <p>app bar</p>
@@ -34,10 +63,16 @@
 export default {
   data() {
     return {
+      items: [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+          { title: 'Photos', icon: 'mdi-image' },
+          { title: 'About', icon: 'mdi-help-box' },
+        ],
+        right: null,
       message: "RMUTL",
-    };
+    }
   },
-};
+}
 </script>
 
 <style>
