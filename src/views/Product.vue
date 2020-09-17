f925436dc3bc16dc65d4b334f0a07cbbe706e551
Phuminan Wimano <oatzapao56@gmail.com>
2020-09-17 14:50:12 +0700

    #8 Product edit detail

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -23,7 +23,7 @@
             required
           ></v-text-field
         ></v-col>
- 
+
         <v-col cols="4"
           ><v-text-field
             v-model="n"
@@ -34,19 +34,124 @@
           ></v-text-field
         ></v-col>
       </v-row>
+
       <v-row>
-        <v-col cols="6"
+        <v-col cols="3"
           ><v-text-field
-            v-model="coin"
-            :rules="coinRules"
+            v-model="w1"
+            :rules="w1Rules"
             :counter="10"
-            label="ราคา"
+            label="นํ้าหนักสินค้า"
+            required
+          ></v-text-field
+        ></v-col>
+
+        <v-col cols="3"
+          ><v-text-field
+            v-model="w2"
+            :rules="w2Rules"
+            :counter="10"
+            label="ความกว้าง"
+            required
+          ></v-text-field
+        ></v-col>
+
+        <v-col cols="3"
+          ><v-text-field
+            v-model="w3"
+            :rules="w3Rules"
+            :counter="10"
+            label="ความยาว"
+            required
+          ></v-text-field
+        ></v-col>
+
+        <v-col cols="3"
+          ><v-text-field
+            v-model="h"
+            :rules="hRules"
+            :counter="10"
+            label="ความสูง"
             required
           ></v-text-field
         ></v-col>
       </v-row>
 
+      <v-row>
+        <v-col cols="6"
+          ><v-text-field
+            v-model="coin_1"
+            :rules="coin_1Rules"
+            :counter="10"
+            label="ราคาทุน"
+            required
+          ></v-text-field
+        ></v-col>
+        <v-col cols="6"
+          ><v-text-field
+            v-model="coin_2"
+            :rules="coin_2Rules"
+            :counter="10"
+            label="ราคาขาย"
+            required
+          ></v-text-field
+        ></v-col>
+      </v-row>
 
+      <v-row>
+        <v-col cols="3"
+          ><v-text-field
+            v-model="date1"
+            :rules="date1Rules"
+            :counter="10"
+            label="วันเดือนปีรับเข้า"
+            required
+          ></v-text-field
+        ></v-col>
+
+        <v-col cols="3"
+          ><v-text-field
+            v-model="date2"
+            :rules="date2Rules"
+            :counter="10"
+            label="วันเดือนปีผลิต"
+            required
+          ></v-text-field
+        ></v-col>
+
+        <v-col cols="3"
+          ><v-text-field
+            v-model="date3"
+            :rules="date3Rules"
+            :counter="10"
+            label="วันเดือนปีขายล่าสุด"
+            required
+          ></v-text-field
+        ></v-col>
+      </v-row>
+
+      <v-row>
+        <!--  -->
+        <v-col cols="4">
+          <v-select
+            v-model="select"
+            :items="type"
+            :rules="[v => !!v || 'กรุณาเลือกประเภทสินค้า']"
+            label="ประเภทสินค้า"
+            required
+          ></v-select>
+        </v-col>
+
+        <v-col cols="4"
+          ><v-text-field
+            v-model="detail"
+            :rules="detailules"
+            :counter="300"
+            label="รายละเอียดสินค้า"
+            required
+          ></v-text-field
+        ></v-col>
+      </v-row>
 
       <v-checkbox
         v-model="checkbox"
@@ -68,7 +173,7 @@
           <!-- ตารางแสดงข้อมูล -->
           <v-card>
             <v-card-title>
-              ข้อมูลลูกค้า
+              ข้อมูลสินค้า
               <v-spacer></v-spacer>
               <v-text-field
                 v-model="search"
@@ -113,8 +218,34 @@ export default {
       v => !!v || "กรุณากรอกจำนวน",
       v => (v && v.length <= 10) || "ต้องมีความยาวไม่เกิน 10 หลัก"
     ],
-    coin: "",
-    coinRules: [
+    w: "",
+    w1Rules: [
+      v => !!v || "กรุณากรอกนํ้าหนัก",
+      v => (v && v.length <= 10) || "ต้องมีความยาวไม่เกิน 10 หลัก"
+    ],
+    w2: "",
+    w2Rules: [
+      v => !!v || "กรุณากรอกนความกว้าง",
+      v => (v && v.length <= 10) || "ต้องมีความยาวไม่เกิน 10 หลัก"
+    ],
+    w3: "",
+    w3ules: [
+      v => !!v || "กรุณากรอกความยาว",
+      v => (v && v.length <= 10) || "ต้องมีความยาวไม่เกิน 10 หลัก"
+    ],
+    h: "",
+    hRules: [
+      v => !!v || "กรุณากรอกความสูง",
+      v => (v && v.length <= 10) || "ต้องมีความยาวไม่เกิน 10 หลัก"
+    ],
+    coin_1: "",
+    coin_1Rules: [
+      v => !!v || "กรุณากรอกราคา",
+      v => (v && v.length <= 10) || "ต้องมีความยาวไม่เกิน 10 หลัก"
+    ],
+
+    coin_2: "",
+    coin_2Rules: [
       v => !!v || "กรุณากรอกราคา",
       v => (v && v.length <= 10) || "ต้องมีความยาวไม่เกิน 10 หลัก"
     ],
@@ -125,21 +256,61 @@ export default {
       v => (v && v.length <= 10) || "ต้องมีความยาวไม่เกิน 10 หลัก"
     ],
 
+    date1: "",
+    date1Rules: [
+      v => !!v || "กรุณากรอกวันเดือนปีรับเข้า",
+      v => (v && v.length <= 10) || "ต้องมีความยาวไม่เกิน 10 หลัก"
+    ],
+
+    date2: "",
+    date2Rules: [
+      v => !!v || "กรุณากรอกวันเดือนปีผลิต",
+      v => (v && v.length <= 10) || "ต้องมีความยาวไม่เกิน 10 หลัก"
+    ],
+
+    date3: "",
+    date3Rules: [
+      v => !!v || "กรุณากรอกวันเดือนปีขายล่าสุด",
+      v => (v && v.length <= 10) || "ต้องมีความยาวไม่เกิน 10 หลัก"
+    ],
+
+    detail: "",
+    detailRules: [
+      v => !!v || "กรุณากรอกรายละเอียด",
+      v => (v && v.length <= 300) || "ชื่อต้องมีขนาดไม่เกิน 300 ตัวอักษร"
+    ],
+
     checkboxRules: [v => !!v || "คุณต้องตรวจสอบสินค้าก่อน"],
     select: null,
     items: ["Item 1", "Item 2", "Item 3", "Item 4"],
-    sex: ["ชาย", "หญิง"],
+    type: [
+      "ของเล่น",
+      "เครื่องใช้ไฟฟ้า",
+      "เครื่องครัว",
+      "ของกิน",
+      "เครื่องสำอาง"
+    ],
     checkbox: false,
- 
+
     search: "",
-    headers: [ 
-      {text:"รหัสสินค้า",value:"no_p"},
+    headers: [
+      { text: "รหัสสินค้า", value: "no_p" },
       {
         text: "ชื่อสินค้า",
-        value: "name_p"},
+        value: "name_p"
+      },
       { text: "จำนวนสินค้า", value: "n" },
-      { text: "ราคาสินค้า", value: "coin" },
-
+      { text: "จำนวนนํ้าหนัก", value: "w1" },
+      { text: "ความกว้าง", value: "w2" },
+      { text: "ความยาว", value: "w3" },
+      { text: "ความสูง", value: "h" },
+      { text: "ราคาทุน", value: "coin_1" },
+      { text: "ราคาขาย", value: "coin_2" },
+      { text: "วันเดือนปีรับเข้า", value: "date1" },
+      { text: "วันเดือนปีผลิต", value: "date2" },
+      { text: "วันเดือนปีขายล่าสุด", value: "date3" },
+      { text: "ประเภทสินค้า", value: "type" },
+      { text: "รายละเอียดสินค้า", value: "detail" }
     ],
 
     desserts: []
@@ -162,13 +333,21 @@ export default {
         //   console.log(this.a_province);
 
         this.desserts.push({
-            name_p:`${this.name_p}`,
-            no_p:`${this.no_p}`,
-            n:`${this.n}`,
-            coin:`${this.coin}`,
-            email:`${this.email}`,
-            sex:`${this.select}`,
-            address:`${this.a_number}ม.${this.a_group}ถ.${this.a_road}ซ.${this.a_lane}ต.${this.a_subdis}อ.${this.a_dis}จ.${this.a_province}`,
+          name_p: `${this.name_p}`,
+          no_p: `${this.no_p}`,
+          n: `${this.n}`,
+          w1: `${this.w1}`,
+          w2: `${this.w2}`,
+          w3: `${this.w3}`,
+          h: `${this.h}`,
+
+          coin_1: `${this.coin_1}`,
+          coin_2: `${this.coin_2}`,
+          date1: `${this.date1}`,
+          date2: `${this.date2}`,
+          date3: `${this.date3}`,
+          type: `${this.select}`,
+          detail: `${this.detail}`
         });
       }
     },
